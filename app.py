00f8f5d424717b1bd218a80eeca3ec9060ccb7ed
ibramaf10 <94913110+ibramaf10@users.noreply.github.com>
2023-12-03 10:50:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_vectorstore(text_chunks):
 
 
 def get_conversation_chain(vectorstore):   
-    llm = ChatOpenAI()
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
